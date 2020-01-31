a2d4ddc318381140bc90f4930cd8b60ec27ba1c7
Tatyana <hordienko.tm@gmail.com>
2020-01-31 16:28:18 -0500

    Homework #6

---
+++ JS-lesson-6/src/js/main.js
@@ -0,0 +1,55 @@
+'use strict';
+
+let start = document.getElementById('start'),
+
+    budgetValue = document.getElementsByClassName('budget-value'),
+    daybudgetValue = document.getElementsByClassName('daybudget-value'),
+    levelValue = document.getElementsByClassName('level-value'),
+    expensesValue = document.getElementsByClassName('expenses-value'),
+    optionalexpensesValue = document.getElementsByClassName('optionalexpenses-value'),
+    incomeValue = document.getElementsByClassName('income-value'),
+    monthsavingsValue = document.getElementsByClassName('monthsavings-value'),
+    yearsavingsValue = document.getElementsByClassName('yearsavings-value'),
+    yearValue = document.getElementsByClassName('year-value'),
+    monthValue = document.getElementsByClassName('month-value'),
+    dayValue = document.getElementsByClassName('day-value'),
+
+    expensesItem = document.getElementsByClassName('expenses-item'),
+
+    optionalexpensesBtn = document.getElementsByTagName('optionalexpenses-btn'),
+    countBudgetBtn = document.getElementsByTagName('count-budget-btn'),
+
+    optionalexpensesItem = document.querySelectorAll('.optionalexpenses-item'),
+
+    chooseIncomeLabel = document.querySelector('.choose-income-label'),
+    checksavings = document.querySelector('.checksavings'),
+    sum = document.querySelector('.sum'),
+    percent = document.querySelector('.percent'),
+    year = document.querySelector('.year'),
+    month = document.querySelector('.month'),
+    day = document.querySelector('.day');
+
+console.log(start);
+console.log(budgetValue);
+console.log(daybudgetValue);
+console.log(levelValue);
+console.log(expensesValue);
+console.log(optionalexpensesValue);
+console.log(incomeValue);
+console.log(monthsavingsValue);
+console.log(yearsavingsValue);
+console.log(yearValue);
+console.log(monthValue);
+console.log(dayValue);
+console.log(expensesItem);
+console.log(optionalexpensesBtn);
+console.log(countBudgetBtn);
+console.log(optionalexpensesItem);
+
+console.log(chooseIncomeLabel);
+console.log(checksavings);
+console.log(sum);
+console.log(percent);
+console.log(year);
+console.log(month);
+console.log(day);
